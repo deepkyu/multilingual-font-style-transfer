3bc7e6063035c4747a3446b876d4d72615a55540
deepkyu <song@deepkyu.me>
2022-12-04 18:07:05 +0900

    update google-font results into separate files

---
+++ evaluate-google-font.sh
@@ -0,0 +1,2 @@
+python evaluate.py -g 0,1 \
+-p /data2/hksong/LOG/font/checkpoint/best/googlefont-latest/epoch=1999-step=11999.ckpt
--- evaluate.sh
+++
@@ -1,2 +0,0 @@
-python evaluate.py -g 0,1 \
--p /data2/hksong/LOG/font/checkpoint/ftgan-patch-full/epoch=21-step=64745.ckpt

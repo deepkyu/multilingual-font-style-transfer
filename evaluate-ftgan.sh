77ee080338407c808370588d71705c98b301bd3f
deepkyu <song@deepkyu.me>
2022-12-11 19:28:14 +0900

    update evaluation script

--- evaluate-ftgan.sh
+++ evaluate-ftgan.sh
@@ -1,4 +1,4 @@
 python evaluate.py \
 -c ./config/setting-ftgan.yaml \
 -g 0 \
--p /data2/hksong/LOG/font/checkpoint/best/ftgan-patch-full/epoch=21-step=64745.ckpt
+-p /PATH/TO/ftgan_vanilla_best.ckpt
--- evaluate-google-font.sh
+++ evaluate-google-font.sh
@@ -1,4 +1,4 @@
 python evaluate.py \
 -c ./config/setting-google-font.yaml \
 -g 0 \
--p /data2/hksong/LOG/font/checkpoint/best/googlefont-latest/epoch=1999-step=11999.ckpt
+-p /PATH/TO/google_font_best.ckpt

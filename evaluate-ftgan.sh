e73c920e78d87ef2ae29e14a8248350c9c5de023
deepkyu <song@deepkyu.me>
2022-12-11 19:29:49 +0900

    update default location for checkpoint

--- evaluate-ftgan.sh
+++ evaluate-ftgan.sh
@@ -1,4 +1,4 @@
 python evaluate.py \
 -c ./config/setting-ftgan.yaml \
 -g 0 \
--p /PATH/TO/ftgan_vanilla_best.ckpt
+-p ./pretrained/ftgan_vanilla_best.ckpt
--- evaluate-google-font.sh
+++ evaluate-google-font.sh
@@ -1,4 +1,4 @@
 python evaluate.py \
 -c ./config/setting-google-font.yaml \
 -g 0 \
--p /PATH/TO/google_font_best.ckpt
+-p ./pretrained/google_font_best.ckpt
